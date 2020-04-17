ffb441b1f11405d67e92d6d68d6946a0c015324e
Farzad Majidfayyaz <farzadmf@users.noreply.github.com>
2020-04-17 11:57:41 -0400

    Add more tests for JSON masker

--- pkg/mask/json_masker_test.go
+++ pkg/mask/json_masker_test.go
@@ -7,24 +7,40 @@ import (
 )
 
 func TestJSONMask(t *testing.T) {
-	var masker JSONMasker
-	var output bytes.Buffer
-
-	input := ""
-	config := Config{
-		Writer: &output,
-	}
-
 	t.Run("should mask 'password' by default", func(t *testing.T) {
-		masker = NewJSONMasker([]string{}, false)
-		input = `  "password": "secret"`
-		output = bytes.Buffer{}
-		config.Reader = strings.NewReader(input)
-
-		masker.Mask(config)
-		trimmedOutput := strings.TrimSpace(output.String())
-		if trimmedOutput != `"password": "***"` {
-			t.Errorf("'password' value not masked; got %q", trimmedOutput)
+		masker := NewJSONMasker([]string{}, false)
+		output := getMaskOutputTrimmed(t, masker, `  "password": "secret"`)
+		if output != `"password": "***"` {
+			t.Errorf("'password' value not masked; got '%s'", output)
 		}
 	})
+
+	t.Run("should not mask an unspecified property", func(t *testing.T) {
+		masker := NewJSONMasker([]string{}, false)
+		output := getMaskOutputTrimmed(t, masker, `  "prop": "value"`)
+		if output != `"prop": "value"` {
+			t.Errorf("unspecified property 'prop' should not be masked; got '%s'", output)
+		}
+	})
+
+	t.Run("should mask specified property", func(t *testing.T) {
+		masker := NewJSONMasker([]string{"prop"}, false)
+		output := getMaskOutputTrimmed(t, masker, `  "prop": "value"`)
+		if output != `"prop": "***"` {
+			t.Errorf("property 'prop' should be masked; got '%s'", output)
+		}
+	})
+}
+
+func getMaskOutputTrimmed(t *testing.T, masker Masker, input string) string {
+	t.Helper()
+
+	output := bytes.Buffer{}
+	config := Config{
+		Writer: &output,
+		Reader: strings.NewReader(input),
+	}
+
+	masker.Mask(config)
+	return strings.TrimSpace(output.String())
 }
